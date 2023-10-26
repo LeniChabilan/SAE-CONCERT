cb16b80f1f3befacffe34cd3981ea142e2bd8dd0
LeniChabilan <leni.chabilan@etu.univ-orleans.fr>
2023-10-26 11:05:50 +0200

    ajout de toutes les insertions pour débuter la mise ne relation de la BD et Flask

--- insertConcert.sql
+++ insertConcert.sql
@@ -1,4 +1,3 @@
--- Exemple d'insertion de musiciens
 INSERT INTO MUSICIEN (musicienID, nomMusicien)VALUES
   (1, 'Pierre Dupont'),
   (2, 'Élise Martin'),
@@ -6,7 +5,6 @@ INSERT INTO MUSICIEN (musicienID, nomMusicien)VALUES
   (4, 'Camille Dubois'),
   (5, 'Lucien Lambert');
 
--- Exemple d'insertion de matériel artiste (instruments de musique)
 INSERT INTO MATERIELARTISTE (materielArtisteID, nomMaterielArt, disponible)VALUES
   (1, 'Guitare acoustique', true),
   (2, 'Piano à queue', true),
@@ -15,24 +13,21 @@ INSERT INTO MATERIELARTISTE (materielArtisteID, nomMaterielArt, disponible)VALUE
   (5, 'Violoncelle', true);
 
 
--- Insertion de rôles pour le personnel technique
 INSERT INTO ROLEP (roleID, nomRole)VALUES
   (1, 'Ingénieur du son'),
   (2, 'Éclairagiste'),
   (3, 'Régisseur de scène'),
   (4, 'Technicien de son'),
-  (5, 'Technicien éclairage'),
+  (5, 'Technicien éclairage');
 
 
--- Insertion d'organisations fictives
 INSERT INTO ORGANISATION (nomOrga, motDePasse, typeOrga)VALUES
   ('HarmonieMusicale', 'mdp123', 'Technique'),
   ('BienveillanceArtistique', 'mdp123', 'Bien-être des Artistes'),
   ('SonsEnScène', 'mdp123', 'Technique'),
-  ('CoeurDeScene', 'mdp123', 'Bien-être des Artistes'),
+  ('CoeurDeScene', 'mdp123', 'Bien-être des Artistes');
 
 
--- Insertion de différents types de places
 INSERT INTO TYPEPLACE (typePlaceID, nomPlace, descriptionP)VALUES
   (1, 'VIP', 'Accès privilégié avec vue exceptionnelle sur la scène.'),
   (2, 'Place Asise', 'Des sièges confortables et une bonne vue sur la scène.'),
@@ -42,7 +37,6 @@ INSERT INTO TYPEPLACE (typePlaceID, nomPlace, descriptionP)VALUES
   (6, 'Place pour Personnes à Mobilité Réduite', 'Places spécialement aménagées pour les personnes à mobilité réduite.');
 
 
--- Insertion de salles de concert fictives situées à Lyon
 INSERT INTO LIEU (lieuID, nomVille, adresseL, departement)VALUES
   (1, 'Lyon', 'Salle Harmonie', 69001),
   (2, 'Lyon', 'Palais des Arts', 69002),
@@ -53,25 +47,18 @@ INSERT INTO LIEU (lieuID, nomVille, adresseL, departement)VALUES
   (7, 'Lyon', 'Le Forum Musical', 69007);
 
 
--- Insertion d'artistes fictifs avec numéros aléatoires
 INSERT INTO ARTISTE (artisteID, pseudoArtiste, nomA, prenomA, mailA, DdNA, LdN, adresseA, numSecuriteSociale, numCNI, dateDelivranceCNI, dateExpirationCNI, dansGroupe)VALUES
   (1, 'RockStar123', 'Dupont', 'Jean', 'jean.dupont@gmail.com', '1990-05-15', 'Lyon', '123 Rue de la Musique', 12367890, 456, '2005-06-20', '2025-06-20', TRUE),
   (2, 'GrooveMaster', 'Martin', 'Marie', 'marie.martin@gmail.com', '1985-08-25', 'Lyon', '456 Avenue du Spectacle', 78954321, 987, '2003-04-12', '2023-04-12', TRUE),
   (3, 'JazzCat', 'Lefebvre', 'Paul', 'paul.lefebvre@gmail.com', '1993-03-10', 'Lyon', '789 Boulevard du Son', 23454321, 567, '2010-09-05', '2030-09-05', TRUE),
   (4, 'PopPrincess', 'Dubois', 'Sophie', 'sophie.dubois@gmail.com', '1988-11-03', 'Lyon', '321 Rue des Artistes', 89045678, 654, '2006-07-15', '2026-07-15', FALSE),
   (5, 'MetalHead', 'Bertrand', 'Luc', 'luc.bertrand@gmail.com', '1997-02-17', 'Lyon', '567 Avenue du Rock', 43245678, 123, '2008-12-01', '2028-12-01', TRUE),
-  (6, 'BluesMaestro', 'Roux', 'Alice', 'alice.roux@gmail.com', '1980-07-08', 'Lyon', '234 Boulevard du Blues', 76554321, 876, '2001-03-30', '2021-03-30', TRUE),
-  (7, 'ReggaeVibes', 'Girard', 'Hugo', 'hugo.girard@gmail.com', '1995-09-27', 'Lyon', '987 Rue de la Reggae', 32198765, 987, '2007-11-22', '2027-11-22', TRUE),
+  (6, 'BluesMaestro', 'Roux', 'Alice', 'alice.roux@gmail.com', '1980-07-08', 'Lyon', '234 Boulevard du Blues', 76554321, 365, '2001-03-30', '2021-03-30', TRUE),
+  (7, 'ReggaeVibes', 'Girard', 'Hugo', 'hugo.girard@gmail.com', '1995-09-27', 'Lyon', '987 Rue de la Reggae', 32198765, 542, '2007-11-22', '2027-11-22', TRUE),
   (8, 'ClassicalMaestro', 'Fournier', 'Emma', 'emma.fournier@gmail.com', '1983-04-02', 'Lyon', '678 Avenue des Classiques', 23498765, 765, '2004-08-10', '2024-08-10', TRUE),
   (9, 'IndieSinger', 'Petit', 'Lucas', 'lucas.petit@gmail.com', '1992-12-19', 'Lyon', '345 Rue de Indépendance', 54354321, 876, '2009-06-18', '2029-06-18', FALSE),
-  (10, 'FolkFiddler', 'Moreau', 'Anna', 'anna.moreau@gmail.com', '1998-06-23', 'Lyon', '234 Avenue de la Folk', 87654321, 345, '2011-05-02', '2031-05-02', TRUE),
-  (11, 'ElectronicBeats', 'Roy', 'Léa', 'lea.roy@gmail.com', '1989-10-12', 'Lyon', '789 Rue de lÉlectronique', 99998765, 111, '2002-02-28', '2022-02-28', TRUE),
-  (12, 'SoulSinger', 'Garcia', 'Enzo', 'enzo.garcia@gmail.com', '1991-07-07', 'Lyon', '456 Boulevard de la Soul', 22298765, 333, '2000-10-05', '2020-10-05', TRUE),
-  (13, 'PunkRocker', 'Lévesque', 'Zoé', 'zoe.levesque@gmail.com', '1987-01-31', 'Lyon', '123 Avenue du Punk', 45654321, 999, '2000-02-15', '2020-02-15', TRUE),
-  (14, 'CountryCowboy', 'Perrin', 'Léo', 'leo.perrin@gmail.com', '1996-04-14', 'Lyon', '321 Rue du Country', 55598765, 777, '2013-07-12', '2033-07-12', FALSE),
-  (15, 'RapMaster', 'Mallet', 'Eva', 'eva.mallet@gmail.com', '1986-03-05', 'Lyon', '567 Boulevard du Rap', 77798765, 555, '2000-02-15', '2020-02-15', TRUE);
+  (10, 'FolkFiddler', 'Moreau', 'Anna', 'anna.moreau@gmail.com', '1998-06-23', 'Lyon', '234 Avenue de la Folk', 87654321, 345, '2011-05-02', '2031-05-02', TRUE);
 
--- Insertion de véhicules fictifs
 INSERT INTO VEHICULE (immatriculation, typeVehicule, capacitéV)VALUES
   ('AB-123-CD', 'Voiture', 5),
   ('XY-789-ZW', 'Camion', 10),
@@ -84,19 +71,18 @@ INSERT INTO VEHICULE (immatriculation, typeVehicule, capacitéV)VALUES
   ('OP-456-QR', 'Bus', 35),
   ('LM-123-UV', 'Minibus', 15),
   ('WX-789-CD', 'Bus', 50),
-  ('EF-123-GH', 'Minibus', 15);
+  ('EF-120-GH', 'Minibus', 15);
 
--- Insertion de matériel fictif pour le son et la lumière
 INSERT INTO MATERIEL (materielID, nomMateriel)VALUES
   (1, 'Microphone dynamique'),
   (2, 'Microphone à condensateur'),
   (3, 'Table de mixage 8 canaux'),
   (4, 'Table de mixage 16 canaux'),
   (5, 'Table de mixage numérique'),
-  (6, 'Enceinte passive 12"'),
-  (7, 'Enceinte passive 15"'),
-  (8, 'Enceinte active 10"'),
-  (9, 'Enceinte active 18"'),
+  (6, 'Enceinte passive 12'),
+  (7, 'Enceinte passive 15'),
+  (8, 'Enceinte active 10'),
+  (9, 'Enceinte active 18'),
   (10, 'Amplificateur de puissance 1000W'),
   (11, 'Amplificateur de puissance 2000W'),
   (12, 'Amplificateur de casque'),
@@ -125,8 +111,8 @@ INSERT INTO MATERIEL (materielID, nomMateriel)VALUES
   (35, 'Stroboscope 1000W'),
   (36, 'Stroboscope LED'),
   (37, 'Panneau LED 4x4'),
-  (38, 'Écran de projection 120"'),
-  (39, 'Écran LED 55"'),
+  (38, 'Écran de projection 120'),
+  (39, 'Écran LED 55'),
   (40, 'Ampli casque 4 canaux'),
   (41, 'Caisse de régie'),
   (42, 'Console de mixage numérique 24 canaux'),
@@ -156,11 +142,11 @@ INSERT INTO MATERIEL (materielID, nomMateriel)VALUES
   (66, 'Projecteur LED 400W'),
   (67, 'Stroboscope 1500W'),
   (68, 'Machine à brouillard professionnel'),
-  (69, 'Écran de projection électrique 150"'),
+  (69, 'Écran de projection électrique 150'),
   (70, 'Lyre à LED wash 250W'),
   (71, 'Console éclairage à gradateurs'),
   (72, 'Machine à confettis électrique'),
-  (73, 'Enceinte passive 10"'),
+  (73, 'Enceinte passive 10'),
   (74, 'Ampli casque 16 canaux'),
   (75, 'Microphone chant dynamique'),
   (76, 'Câble alimentation PowerCON'),
@@ -171,7 +157,7 @@ INSERT INTO MATERIEL (materielID, nomMateriel)VALUES
   (81, 'Table de mixage 32 canaux'),
   (82, 'Microphone à ruban'),
   (83, 'Machine à neige artificielle'),
-  (84, 'Écran LED 75"'),
+  (84, 'Écran LED 75'),
   (85, 'Amplificateur de casque 8 canaux'),
   (86, 'Console de mixage numérique 48 canaux'),
   (87, 'Machine à fumée lourde professionnelle'),
@@ -185,16 +171,16 @@ INSERT INTO MATERIEL (materielID, nomMateriel)VALUES
   (95, 'Câble jack 3.5mm 3m'),
   (96, 'Câble adaptateur XLR-jack 6.35mm'),
   (97, 'Câble speakon 50m'),
-  (98, 'Écran de projection motorisé 200"'),
+  (98, 'Écran de projection motorisé 200'),
   (99, 'Lyre beam 200W'),
   (100, 'Lyre wash 180W'),
   (101, 'Console de mixage compacte 12 canaux'),
-  (102, 'Enceinte active 8"'),
+  (102, 'Enceinte active 8'),
   (103, 'Caisse de régie avec rack'),
   (104, 'Microphone dynamique instrument'),
   (105, 'Machine à confettis manuelle'),
   (106, 'Ampli casque 12 canaux'),
-  (107, 'Écran LED 85"'),
+  (107, 'Écran LED 85'),
   (108, 'Lyre à LED wash spot 350W'),
   (109, 'Machine à brouillard lourd professionnelle'),
   (110, 'Table de mixage 24 canaux'),
@@ -210,12 +196,12 @@ INSERT INTO MATERIEL (materielID, nomMateriel)VALUES
   (120, 'Console éclairage à gradateurs 48 canaux'),
   (121, 'Lyre à LED wash beam 350W'),
   (122, 'Ampli de puissance 5000W'),
-  (123, 'Écran de projection électrique 250"'),
+  (123, 'Écran de projection électrique 250'),
   (124, 'Lyre spot wash 180W'),
   (125, 'Console de mixage numérique 64 canaux'),
   (126, 'Lyre à LED beam spot 400W'),
   (127, 'Machine à mousse professionnelle'),
-  (128, 'Enceinte passive 15"'),
+  (128, 'Enceinte passive 15'),
   (129, 'Ampli casque 24 canaux'),
   (130, 'Microphone studio à ruban'),
   (131, 'Câble alimentation PowerCON TRUE1'),
@@ -223,7 +209,7 @@ INSERT INTO MATERIEL (materielID, nomMateriel)VALUES
   (133, 'Lyre à LED spot beam 250W'),
   (134, 'Machine à brouillard haute densité professionnelle'),
   (135, 'Console de mixage analogique 48 canaux'),
-  (136, 'Écran LED 100"'),
+  (136, 'Écran LED 100'),
   (137, 'Amplificateur de casque 16 canaux'),
   (138, 'Machine à confettis électrique à grande capacité'),
   (139, 'Lyre laser RGB 4W'),
@@ -235,7 +221,7 @@ INSERT INTO MATERIEL (materielID, nomMateriel)VALUES
   (145, 'Lyre à LED spot wash beam 350W'),
   (146, 'Machine à fumée basse densité professionnelle avec télécommande'),
   (147, 'Console de mixage compacte 16 canaux'),
-  (148, 'Enceinte active 12"'),
+  (148, 'Enceinte active 12'),
   (149, 'Lyre à LED wash spot beam 500W'),
   (150, 'Machine à mousse lourde professionnelle');
 
@@ -244,22 +230,17 @@ INSERT INTO GROUPE (groupeID, nomGroupe)VALUES
   (1, 'Les Rockeurs'),
   (2, 'PopPrincess'),
   (3, 'Pop Sensation'),
-  (4, 'IndieSinger'),
-  (5, 'Heavy Metal Legends'),
-  (6, 'CountryCowboy'),
+  (4, 'IndieSinger');
 
--- Insertion de salles fictives
+
 INSERT INTO SALLE (salleID, nomSalle, capaciteTotaleSalle, planSalle, dimensionOuverture, dimensionProfondeur, lieuID)VALUES
   (1, 'Salle A', 500, NULL, 20.5, 30.2, 1), -- Lieu ID 1 (Lyon Salle 1)
   (2, 'Salle B', 300, NULL, 15.8, 25.4, 2), -- Lieu ID 2 (Lyon Salle 2)
   (3, 'Salle C', 700, NULL, 25.0, 35.6, 3), -- Lieu ID 3 (Lyon Salle 3)
-  (4, 'Salle D', 400, NULL, 18.2, 27.9, 4), -- Lieu ID 4 (Lyon Salle 4)
-  (5, 'Salle E', 250, NULL, 12.6, 22.1, 5); -- Lieu ID 5 (Lyon Salle 5)
-  (6, 'Salle F', 450, NULL, 17.3, 28.7, 6), -- Lieu ID 6 (Lyon Salle 6)
-  (7, 'Salle G', 550, NULL, 19.9, 32.1, 7), -- Lieu ID 7 (Lyon Salle 7)
+  (4, 'Salle D', 400, NULL, 18.2, 27.9, 4); -- Lieu ID 4 (Lyon Salle 4)
 
-  -- Insertion de membres du personnel technique fictifs
-INSERT INTO PERSONNELTECHNIQUE (personelTechniqueID, roleID, nomP, prenomP)VALUES
+
+INSERT INTO PERSONELTECHNIQUE(personelTechniqueID, roleID, nomP, prenomP)VALUES
   (1, 1, 'Martin', 'Dupont'), -- Ingénieur du son
   (2, 2, 'Sophie', 'Lefebvre'), -- Éclairagiste
   (3, 3, 'Philippe', 'Roux'), -- Régisseur de scène
@@ -272,3 +253,208 @@ INSERT INTO PERSONNELTECHNIQUE (personelTechniqueID, roleID, nomP, prenomP)VALUE
   (10, 5, 'Sophie', 'Tremblay'), -- Technicien éclairage
   (11, 1, 'Thomas', 'Leroy'), -- Ingénieur du son
   (12, 2, 'Léa', 'Dubois'); -- Éclairagiste
+
+INSERT INTO PLAN (planID, planScene, planFeu, salleID)VALUES
+  (1, NULL, NULL, 1),
+  (2, NULL, NULL, 2),
+  (3, NULL, NULL, 3),
+  (4, NULL, NULL, 4),
+  (5, NULL, NULL, 1),
+  (6, NULL, NULL, 2),
+  (7, NULL, NULL, 3),
+  (8, NULL, NULL, 4);
+
+INSERT INTO HEBERGMENT (hebergementID, nomHebergement, capacitéH, qualitéH, lieuID, prix)VALUES
+  (1, 'Hôtel de Lyon', 100, 4, 1, 150),
+  (2, 'Auberge des Musiciens', 50, 3, 2, 80),
+  (3, 'Appartement du Vieux Lyon', 30, 5, 1, 120),
+  (4, 'Chambre d hôtes du Vieux Lyon', 10, 4, 1, 70),
+  (5, 'Hôtel Belle Vue', 80, 4, 3, 130),
+  (6, 'Maison de Ville Lumière', 45, 3, 2, 90);
+
+INSERT INTO MATERIELSALLE (materielSalleID, salleID, nomMaterielS, disponible)VALUES
+  (1, 1, 'Microphone dynamique', true),
+  (2, 1, 'Microphone à condensateur', true),
+  (3, 1, 'Table de mixage 8 canaux', true),
+  (4, 1, 'Table de mixage 16 canaux', false),
+  (5, 1, 'Table de mixage numérique', true),
+  (6, 2, 'Enceinte passive 12', true),
+  (7, 2, 'Enceinte passive 15', false),
+  (8, 2, 'Enceinte active 10', true),
+  (9, 2, 'Enceinte active 18', true),
+  (10, 2, 'Amplificateur de puissance 1000W', true),
+  (11, 3, 'Amplificateur de puissance 2000W', true),
+  (12, 3, 'Amplificateur de casque', false),
+  (13, 3, 'Casque écoute professionnel', true),
+  (14, 3, 'Effets audio rackables', true),
+  (15, 3, 'Laser RGB 1W', false),
+  (16, 4, 'Projecteur LED 100W', true),
+  (17, 4, 'Projecteur LED 200W', true),
+  (18, 4, 'Machine à brouillard', true),
+  (19, 4, 'Machine à fumée lourde', false),
+  (20, 4, 'Lyre à LED 60W', true);
+
+
+
+INSERT INTO CONCERT (concertID, nomConcert, dateDebutConcert, dateFinConcert, ficheTechnique, catering, salleID, groupeID) VALUES
+  (1, 'Concert 1', '2023-11-28', '2023-11-29', 'Fiche technique du concert 1', 'Catering du concert 1', 2, 2),
+  (2, 'Concert 2', '2023-11-27', '2023-11-28', 'Fiche technique du concert 2', 'Catering du concert 2', 3, 1),
+  (3, 'Concert 3', '2023-12-23', '2023-12-24', 'Fiche technique du concert 3', 'Catering du concert 3', 1, 3);
+
+
+INSERT INTO MUSICIENADDITIONEL(musicienID, concertID)VALUES
+  (1, 1), 
+  (2, 1), 
+  (3, 1), 
+  (4, 2), 
+  (5, 2), 
+  (4, 3), 
+  (1, 3); 
+
+INSERT INTO TRANSPORTE (concertID, immatriculation)VALUES
+  (1, 'AB-123-CD'),
+  (2, 'EF-123-GH'),
+  (3, 'CD-123-EF');
+
+
+INSERT INTO COMPOSER (artisteID, groupeID)VALUES
+  (1, 1), 
+  (2, 1),
+  (3, 1),
+  (4, 2), 
+  (9, 4), 
+  (7, 3), 
+  (5, 3); 
+
+
+INSERT INTO NECESSITER (materielID, concertID)VALUES
+  (1, 1), 
+  (2, 1),
+  (3, 2), 
+  (4, 2),
+  (5, 2), 
+  (6, 2), 
+  (7, 2), 
+  (8, 2), 
+  (9, 2), 
+  (10, 1), 
+  (11, 1), 
+  (12, 1), 
+  (13, 1), 
+  (14, 1), 
+  (15, 1), 
+  (16, 1), 
+  (17, 1), 
+  (18, 1), 
+  (19, 1), 
+  (20, 1), 
+  (21, 1), 
+  (22, 1), 
+  (23, 1), 
+  (24, 1), 
+  (25, 2), 
+  (26, 2), 
+  (27, 2), 
+  (28, 2), 
+  (29, 2), 
+  (30, 2), 
+  (31, 2), 
+  (32, 2), 
+  (33, 2), 
+  (34, 2), 
+  (35, 2), 
+  (36, 2), 
+  (37, 2), 
+  (38, 2), 
+  (39, 2), 
+  (40, 3), 
+  (41, 3), 
+  (42, 3), 
+  (43, 3), 
+  (44, 3), 
+  (45, 3), 
+  (46, 3), 
+  (47, 3), 
+  (48, 3), 
+  (49, 3), 
+  (50, 3), 
+  (51, 3), 
+  (52, 3), 
+  (53, 3), 
+  (54, 3), 
+  (55, 3), 
+  (56, 3), 
+  (57, 3), 
+  (58, 3), 
+  (59, 3);
+  
+INSERT INTO UTILISE (materielArtisteID, groupeID, quantiteMaterielArt)
+VALUES
+  (1, 1, 2),  -- Les Rockeurs utilisent 2 guitares acoustiques
+  (2, 2, 1),  -- PopPrincess utilise 1 piano à queue
+  (3, 1, 1),  -- Les Rockeurs utilisent 1 batterie complète
+  (4, 3, 3),  -- Pop Sensation utilise 3 saxophones alto
+  (5, 4, 2);  -- IndieSinger utilise 2 violoncelles
+
+
+INSERT INTO PARTICIPE (concertId, groupeID)VALUES
+  (1, 1), -- Groupe ID 1 participe au Concert ID 1
+  (2, 2), -- Groupe ID 2 participe au Concert ID 2
+  (3, 3); -- Groupe ID 3 participe au Concert ID 3
+
+
+
+INSERT INTO PREPARE (personelTechniqueID, concertID)VALUES
+  (1, 1),  -- Martin Dupont prépare Concert 1
+  (2, 1),  -- Sophie Lefebvre prépare Concert 1
+  (3, 1),  -- Philippe Roux prépare Concert 1
+  (4, 2),  -- Camille Moreau prépare Concert 2
+  (5, 2),  -- Éric Lambert prépare Concert 2
+  (6, 3),  -- Isabelle Gagnon prépare Concert 3
+  (7, 3),  -- Antoine Lapointe prépare Concert 3
+  (8, 3),  -- David Bergeron prépare Concert 3
+  (9, 1),  -- Valérie Leclerc prépare Concert 1
+  (10, 1), -- Sophie Tremblay prépare Concert 1
+  (11, 2), -- Thomas Leroy prépare Concert 2
+  (12, 3); -- Léa Dubois prépare Concert 3
+
+
+INSERT INTO SALLETYPEPLACE (typePlaceID, salleID, capaciteS)
+VALUES
+  (1, 1, 50),  -- Capacité VIP dans Salle A
+  (2, 1, 200),  -- Capacité Place Assise dans Salle A
+  (3, 1, 300),  -- Capacité Place en Fosse dans Salle A
+  (4, 1, 400),  -- Capacité Place Debout dans Salle A
+  (5, 1, 30),  -- Capacité Place Enfant dans Salle A
+  (6, 1, 10),  -- Capacité Place pour Personnes à Mobilité Réduite dans Salle A
+
+  (1, 2, 30),  -- Capacité VIP dans Salle B
+  (2, 2, 150),  -- Capacité Place Assise dans Salle B
+  (3, 2, 200),  -- Capacité Place en Fosse dans Salle B
+  (4, 2, 250),  -- Capacité Place Debout dans Salle B
+  (5, 2, 20),  -- Capacité Place Enfant dans Salle B
+  (6, 2, 8),  -- Capacité Place pour Personnes à Mobilité Réduite dans Salle B
+
+  (1, 3, 70),  -- Capacité VIP dans Salle C
+  (2, 3, 350),  -- Capacité Place Assise dans Salle C
+  (3, 3, 450),  -- Capacité Place en Fosse dans Salle C
+  (4, 3, 600),  -- Capacité Place Debout dans Salle C
+  (5, 3, 40),  -- Capacité Place Enfant dans Salle C
+  (6, 3, 12),  -- Capacité Place pour Personnes à Mobilité Réduite dans Salle C
+
+  (1, 4, 40),  -- Capacité VIP dans Salle D
+  (2, 4, 200),  -- Capacité Place Assise dans Salle D
+  (3, 4, 250),  -- Capacité Place en Fosse dans Salle D
+  (4, 4, 300),  -- Capacité Place Debout dans Salle D
+  (5, 4, 25),  -- Capacité Place Enfant dans Salle D
+  (6, 4, 6);  -- Capacité Place pour Personnes à Mobilité Réduite dans Salle D
+
+
+INSERT INTO ORGANISER (concertID, nomOrga) VALUES
+  (1, 'HarmonieMusicale'), 
+  (1, 'BienveillanceArtistique'), 
+  (2, 'SonsEnScène'),
+  (2, 'CoeurDeScene'), 
+  (3, 'HarmonieMusicale'),
+  (3, 'CoeurDeScene');
+
