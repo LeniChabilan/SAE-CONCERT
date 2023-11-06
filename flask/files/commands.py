2dc1134c5b57438dd1e2c6334e2ee804fc6059fa
Debray Antoine <o22202147@info23-21.iut45.univ-orleans.fr>
2023-11-06 16:33:51 +0100

    modification des modeles pour corespondre au MCD et mise en forme des datas du YML et creation de la commande loaddb réaliser par Leni et Antoine

--- flask/files/commands.py
+++ flask/files/commands.py
@@ -1,28 +1,221 @@
 import click
 from .app import app,db
+from datetime import datetime
 
 @app.cli.command()
 @click.argument('filename')
 
+
+# def loaddb(filename):
+#     '''Crée les tables et les remplit de données'''
+#     db.create_all()
+#     # Chargement du fichier YAML
+#     import yaml
+#     data = yaml.safe_load(open(filename))
+#     from .models import Musicien 
+#     # Vérifiez que le fichier contient la clé "Musicien"
+#     print(len(data))
+#     for nomTable in data:
+#         print(nomTable)
+#         if "Musicien" in nomTable:
+#             musiciens = nomTable["Musicien"]
+#             print("ok1")
+#             # Itération sur la liste de musiciens dans le fichier YAML
+#             for musicien_data in musiciens:
+#                 print("ok2")
+#                 if "musicienID" in musicien_data and "nomMusicien" in musicien_data:
+#                     musicien_id = musicien_data["musicienID"]
+#                     nom_musicien = musicien_data["nomMusicien"]
+                    
+#                     # Crée un objet Musicien et l'ajoute à la session
+#                     musicien = Musicien(musicienID=musicien_id, nomMusicien=nom_musicien)
+#                     db.session.add(musicien)
+#     db.session.commit()
+
+
+
 def loaddb(filename):
-    '''Creates the tables and populates them with data'''
-    #créationdetouteslestables
+    '''Crée les tables et les remplit de données'''
     db.create_all()
-    #chargementdenotrejeudedonnées
     import yaml
-    file = yaml.safe_load(open(filename))
-    #importdesmodèles
-    from .models import Organisation
-    #premièrepasse:créationdetouslesauteurs
-    organisations={}
-    for o in file:
-        nom=o["nomOrga"]
-        mdp = o["motDePasse"]
-        type = o["typeOrga"]
-        if nom not in organisations:
-            org = Organisation(nomOrga=nom, motDePasse=mdp, typeOrga=type)
-            db.session.add(org)
+    data = yaml.safe_load(open(filename))
+    from .models import Musicien, MaterielArtiste, RoleP, Organisation, TypePlace, Lieu, Artiste, Vehicule, Materiel, Groupe, Salle, PersonelTechnique, Plan, Hebergement, MaterielSalle, Concert, MusicienAdditionnel, Transporte, Necessiter, Composer, Utilise, Participe, Prepare, SalleTypePlace, Organiser
+
+    for nomTable in data:
+        if "Musicien" in nomTable:
+            musiciens = nomTable["Musicien"]
+            for musicien_data in musiciens:
+                musicien = Musicien(musicienID=musicien_data["musicienID"], nomMusicien=musicien_data["nomMusicien"])
+                db.session.add(musicien)
+
+        if "MaterielArtiste" in nomTable:
+            materiels = nomTable["MaterielArtiste"]
+            for materiel_data in materiels:
+                materiel = MaterielArtiste(materielArtisteID=materiel_data["materielArtisteID"], nomMaterielArt=materiel_data["nomMaterielArt"], disponible=materiel_data["disponible"])
+                db.session.add(materiel)
+
+        if "RoleP" in nomTable:
+            roles = nomTable["RoleP"]
+            for role_data in roles:
+                role = RoleP(roleID=role_data["roleID"], nomRole=role_data["nomRole"])
+                db.session.add(role)
+
+        if "Organisation" in nomTable:
+            organisations = nomTable["Organisation"]
+            for organisation_data in organisations:
+                organisation = Organisation(nomOrga=organisation_data["nomOrga"], motDePasse=organisation_data["motDePasse"], typeOrga=organisation_data["typeOrga"])
+                db.session.add(organisation)
+
+        if "TypePlace" in nomTable:
+            typePlaces = nomTable["TypePlace"]
+            for typePlace_data in typePlaces:
+                typePlace = TypePlace(typePlaceID=typePlace_data["typePlaceID"], nomPlace=typePlace_data["nomPlace"], descriptionP=typePlace_data["descriptionP"])
+                db.session.add(typePlace)
+
+        if "Lieu" in nomTable:
+            lieux = nomTable["Lieu"]
+            for lieu_data in lieux:
+                lieu = Lieu(lieuID=lieu_data["lieuID"], nomVille=lieu_data["nomVille"], adresseL=lieu_data["adresseL"], departement=lieu_data["departement"])
+                db.session.add(lieu)
+
+        if "Artiste" in nomTable:
+            artistes = nomTable["Artiste"]
+            for artiste_data in artistes:
+                artiste = Artiste(artisteID=artiste_data["artisteID"], pseudoArtiste=artiste_data["pseudoArtiste"], nomA=artiste_data["nomA"], prenomA=artiste_data["prenomA"],mailA=artiste_data["mailA"], DdNA=datetime.strptime(artiste_data["DdNA"],"%Y-%m-%d").date(), LdN=artiste_data["LdN"], adresseA=artiste_data["adresseA"], numSecuriteSociale=artiste_data["numSecuriteSociale"], numCNI=artiste_data["numCNI"], dateDelivranceCNI=datetime.strptime(artiste_data["dateDelivranceCNI"],"%Y-%m-%d").date(),dateExpirationCNI=datetime.strptime(artiste_data["dateExpirationCNI"],"%Y-%m-%d").date(),dansGroupe=artiste_data["dansGroupe"])
+                db.session.add(artiste)
+
+        if "Vehicule" in nomTable:
+            vehicules = nomTable["Vehicule"]
+            for vehicule_data in vehicules:
+                vehicule = Vehicule(immatriculation=vehicule_data["immatriculation"], typeVehicule=vehicule_data["typeVehicule"], capacitéV=vehicule_data["capacitéV"])
+                db.session.add(vehicule)
+
+        if "Materiel" in nomTable:
+            materiels = nomTable["Materiel"]
+
+            for materiel_data in materiels:
+                materiel = Materiel(materielID=materiel_data["materielID"], nomMateriel=materiel_data["nomMateriel"])
+                db.session.add(materiel)
+
+        if "Groupe" in nomTable:
+            groupes = nomTable["Groupe"]
+            for groupe_data in groupes:
+                groupe = Groupe(groupeID=groupe_data["groupeID"], nomGroupe=groupe_data["nomGroupe"])
+                db.session.add(groupe)
+
+        if "Salle" in nomTable:
+            salles = nomTable["Salle"]
+            for salle_data in salles:
+                salle = Salle(salleID=salle_data["salleID"], nomSalle=salle_data["nomSalle"], capaciteTotaleSalle=salle_data["capaciteTotaleSalle"], planSalle=salle_data["planSalle"], dimensionOuverture=salle_data["dimensionOuverture"], dimensionProfondeur=salle_data["dimensionProfondeur"], lieuID=salle_data["lieuID"])
+                db.session.add(salle)
+
+        if "PersonelTechnique" in nomTable:
+            personels = nomTable["PersonelTechnique"]
+            for personel_data in personels:
+                personel = PersonelTechnique(personelTechniqueID=personel_data["personelTechniqueID"], roleID=personel_data["roleID"], nomP=personel_data["nomP"], prenomP=personel_data["prenomP"])
+                db.session.add(personel)
+
+        if "Plan" in nomTable:
+            plans = nomTable["Plan"]
+            for plan_data in plans:
+                plan = Plan(planID=plan_data["planID"], planScene=plan_data["planScene"], planFeu=plan_data["planFeu"], salleID=plan_data["salleID"])
+                db.session.add(plan)
+
+        if "Hebergment" in nomTable:
+            hebergments = nomTable["Hebergment"]
+            for hebergment_data in hebergments:
+                hebergment = Hebergement(hebergementID=hebergment_data["hebergementID"], nomHebergement=hebergment_data["nomHebergement"], capacitéH=hebergment_data["capacitéH"], qualitéH=hebergment_data["qualitéH"], lieuID=hebergment_data["lieuID"])
+                db.session.add(hebergment)
+
+        if "MaterielSalle" in nomTable:
+            materiels = nomTable["MaterielSalle"]
+            for materiel_data in materiels:
+                materiel = MaterielSalle(materielSalleID=materiel_data["materielSalleID"], salleID=materiel_data["salleID"], nomMaterielS=materiel_data["nomMaterielS"], disponible=materiel_data["disponible"])
+                db.session.add(materiel)
+
+        if "Concert" in nomTable:
+            concerts = nomTable["Concert"]
+            for concert_data in concerts:
+                concert = Concert(concertID=concert_data["concertID"], nomConcert=concert_data["nomConcert"], dateDebutConcert=datetime.strptime(concert_data["dateDebutConcert"],"%Y-%m-%d").date(), dateFinConcert=datetime.strptime(concert_data["dateFinConcert"],"%Y-%m-%d").date(), ficheTechnique=concert_data["ficheTechnique"], catering=concert_data["catering"], salleID=concert_data["salleID"], groupeID=concert_data["groupeID"])
+                db.session.add(concert)
+
+        if "MusicienAdditionnel" in nomTable:
+            musiciens = nomTable["MusicienAdditionnel"]
+            for musicien_data in musiciens:
+                musicien = MusicienAdditionnel(musicienID=musicien_data["musicienID"], concertID=musicien_data["concertID"])
+                db.session.add(musicien)
+
+        if "Transporte" in nomTable:
+            transportes = nomTable["Transporte"]
+            for transport_data in transportes:
+                transporte = Transporte(concertID=transport_data["concertID"], immatriculation=transport_data["immatriculation"])
+                db.session.add(transporte)
+
+        if "Necessiter" in nomTable:
+            necessites = nomTable["Necessiter"]
+            for necessite_data in necessites:
+                necessite = Necessiter(materielID=necessite_data["materielID"], concertID=necessite_data["concertID"])
+                db.session.add(necessite)
+
+        if "Composer" in nomTable:
+            compositions = nomTable["Composer"]
+            for composition_data in compositions:
+                composition = Composer(artisteID=composition_data["artisteID"], groupeID=composition_data["groupeID"])
+                db.session.add(composition)
+
+        if "Utilise" in nomTable:
+            utilisations = nomTable["Utilise"]
+            for utilisation_data in utilisations:
+                utilisation = Utilise(materielArtisteID=utilisation_data["materielArtisteID"], groupeID=utilisation_data["groupeID"], quantiteMaterielArt=utilisation_data["quantiteMaterielArt"])
+                db.session.add(utilisation)
+
+        if "PARTICIPE" in nomTable:
+            participations = nomTable["PARTICIPE"]
+            for participation_data in participations:
+                participation = Participe(concertID=participation_data["concertId"], groupeID=participation_data["groupeID"])
+                db.session.add(participation)
+
+        if "Prepare" in nomTable:
+            preparations = nomTable["Prepare"]
+            for preparation_data in preparations:
+                preparation = Prepare(personelTechniqueID=preparation_data["personelTechniqueID"], concertID=preparation_data["concertID"])
+                db.session.add(preparation)
+
+        if "SALLETYPEPLACE" in nomTable:
+            salleTypePlaces = nomTable["SALLETYPEPLACE"]
+            for salleTypePlace_data in salleTypePlaces:
+                salleTypePlace = SalleTypePlace(typePlaceID=salleTypePlace_data["typePlaceID"], salleID=salleTypePlace_data["salleID"], capaciteS=salleTypePlace_data["capaciteS"])
+                db.session.add(salleTypePlace)
+
+        if "ORGANISER" in nomTable:
+            organisateurs = nomTable["ORGANISER"]
+            for organisateur_data in organisateurs:
+                organisateur = Organiser(concertID=organisateur_data["concertID"], nomOrga=organisateur_data["nomOrga"])
+                db.session.add(organisateur)
+
     db.session.commit()
+
+
+
+# def loaddb(filename):
+#     '''Creates the tables and populates them with data'''
+#     #créationdetouteslestables
+#     db.create_all()
+#     #chargementdenotrejeudedonnées
+#     import yaml
+#     file = yaml.safe_load(open(filename))
+#     #importdesmodèles
+#     from .models import Organisation
+#     #premièrepasse:créationdetouslesauteurs
+#     organisations={}
+#     for o in file:
+#         nom=o["nomOrga"]
+#         mdp = o["motDePasse"]
+#         type = o["typeOrga"]
+#         if nom not in organisations:
+#             org = Organisation(nomOrga=nom, motDePasse=mdp, typeOrga=type)
+#             db.session.add(org)
+#     db.session.commit()
 
 @app.cli.command()
 def syncdb():
--- flask/files/models.py
+++ flask/files/models.py
@@ -13,18 +13,18 @@ from .app import login_manager
 def load_user(nomOrga):
     return Organisation.query.get(nomOrga)
 
-class Musicien(Base):
+class Musicien(db.Model):
     __tablename__ = 'MUSICIEN'
     musicienID = Column(Integer, primary_key=True)
     nomMusicien = Column(String(50))
 
-class MaterielArtiste(Base):
+class MaterielArtiste(db.Model):
     __tablename__ = 'MATERIELARTISTE'
     materielArtisteID = Column(Integer, primary_key=True)
     nomMaterielArt = Column(String(200))
     disponible = Column(Boolean)
 
-class RoleP(Base):
+class RoleP(db.Model):
     __tablename__ = 'ROLEP'
     roleID = Column(Integer, primary_key=True)
     nomRole = Column(String(50))
@@ -38,20 +38,20 @@ class Organisation(db.Model, UserMixin):
     def get_id(self):
         return self.nomOrga
 
-class TypePlace(Base):
+class TypePlace(db.Model):
     __tablename__ = 'TYPEPLACE'
     typePlaceID = Column(Integer, primary_key=True)
     nomPlace = Column(String(50))
     descriptionP = Column(Text)
 
-class Lieu(Base):
+class Lieu(db.Model):
     __tablename__ = 'LIEU'
     lieuID = Column(Integer, primary_key=True)
     nomVille = Column(String(255))
     adresseL = Column(String(255))
     departement = Column(Integer)
 
-class Artiste(Base):
+class Artiste(db.Model):
     __tablename__ = 'ARTISTE'
     artisteID = Column(Integer, primary_key=True)
     pseudoArtiste = Column(String(255))
@@ -67,23 +67,23 @@ class Artiste(Base):
     dateExpirationCNI = Column(Date)
     dansGroupe = Column(Boolean)
 
-class Vehicule(Base):
+class Vehicule(db.Model):
     __tablename__ = 'VEHICULE'
     immatriculation = Column(String(255), primary_key=True)
     typeVehicule = Column(String(255))
     capacitéV = Column(Integer)
 
-class Materiel(Base):
+class Materiel(db.Model):
     __tablename__ = 'MATERIEL'
     materielID = Column(Integer, primary_key=True)
     nomMateriel = Column(String(255))
 
-class Groupe(Base):
+class Groupe(db.Model):
     __tablename__ = 'GROUPE'
     groupeID = Column(Integer, primary_key=True)
     nomGroupe = Column(String(255))
 
-class Salle(Base):
+class Salle(db.Model):
     __tablename__ = 'SALLE'
     salleID = Column(Integer, primary_key=True)
     nomSalle = Column(String(255))
@@ -94,7 +94,7 @@ class Salle(Base):
     lieuID = Column(Integer, ForeignKey('LIEU.lieuID'))
     lieu = relationship(Lieu)
 
-class PersonelTechnique(Base):
+class PersonelTechnique(db.Model):
     __tablename__ = 'PERSONELTECHNIQUE'
     personelTechniqueID = Column(Integer, primary_key=True)
     roleID = Column(Integer, ForeignKey('ROLEP.roleID'))
@@ -102,7 +102,7 @@ class PersonelTechnique(Base):
     prenomP = Column(String(50))
     role = relationship(RoleP)
 
-class Plan(Base):
+class Plan(db.Model):
     __tablename__ = 'PLAN'
     planID = Column(Integer, primary_key=True)
     planScene = Column(Integer)  # Utilisez le type de données approprié pour BLOB
@@ -110,7 +110,7 @@ class Plan(Base):
     salleID = Column(Integer, ForeignKey('SALLE.salleID'))
     salle = relationship(Salle)
 
-class Hebergement(Base):
+class Hebergement(db.Model):
     __tablename__ = 'HEBERGMENT'
     hebergementID = Column(Integer, primary_key=True, autoincrement=True)
     nomHebergement = Column(String(255), nullable=False)
@@ -119,7 +119,7 @@ class Hebergement(Base):
     lieuID = Column(Integer, ForeignKey('LIEU.lieuID'))
     lieu = relationship(Lieu)
 
-class MaterielSalle(Base):
+class MaterielSalle(db.Model):
     __tablename__ = 'MATERIELSALLE'
     materielSalleID = Column(Integer, primary_key=True)
     salleID = Column(Integer, ForeignKey('SALLE.salleID'))
@@ -127,7 +127,7 @@ class MaterielSalle(Base):
     disponible = Column(Boolean)
     salle = relationship(Salle)
 
-class Concert(Base):
+class Concert(db.Model):
     __tablename__ = 'CONCERT'
     concertID = Column(Integer, primary_key=True)
     nomConcert = Column(String(255))
@@ -140,36 +140,35 @@ class Concert(Base):
     salle = relationship(Salle)
     groupe = relationship(Groupe)
 
-class MusicienAdditionnel(Base):
+class MusicienAdditionnel(db.Model):
     __tablename__ = 'MUSICIENADDITIONEL'
     musicienID = Column(Integer, ForeignKey('MUSICIEN.musicienID'), primary_key=True)
     concertID = Column(Integer, ForeignKey('CONCERT.concertID'), primary_key=True)
     musicien = relationship(Musicien)
     concert = relationship(Concert)
 
-class Transporte(Base):
+class Transporte(db.Model):
     __tablename__ = 'TRANSPORTE'
     concertID = Column(Integer, ForeignKey('CONCERT.concertID'), primary_key=True)
     immatriculation = Column(String(50), ForeignKey('VEHICULE.immatriculation'), primary_key=True)
     concert = relationship(Concert)
     vehicule = relationship(Vehicule)
 
-class Necessiter(Base):
+class Necessiter(db.Model):
     __tablename__ = 'NECESSITER'
     materielID = Column(Integer, ForeignKey('MATERIEL.materielID'), primary_key=True)
     concertID = Column(Integer, ForeignKey('CONCERT.concertID'), primary_key=True)
-    quantiteM = Column(Integer)
     materiel = relationship(Materiel)
     concert = relationship(Concert)
 
-class Composer(Base):
+class Composer(db.Model):
     __tablename__ = 'COMPOSER'
     artisteID = Column(Integer, ForeignKey('ARTISTE.artisteID'), primary_key=True)
     groupeID = Column(Integer, ForeignKey('GROUPE.groupeID'), primary_key=True)
     artiste = relationship(Artiste)
     groupe = relationship(Groupe)
 
-class Utilise(Base):
+class Utilise(db.Model):
     __tablename__ = 'UTILISE'
     materielArtisteID = Column(Integer, ForeignKey('MATERIELARTISTE.materielArtisteID'), primary_key=True)
     groupeID = Column(Integer, ForeignKey('GROUPE.groupeID'), primary_key=True)
@@ -177,29 +176,22 @@ class Utilise(Base):
     materielArtiste = relationship(MaterielArtiste)
     groupe = relationship(Groupe)
 
-class Participe(Base):
+class Participe(db.Model):
     __tablename__ = 'PARTICIPE'
     concertID = Column(Integer, ForeignKey('CONCERT.concertID'), primary_key=True)
-    artisteID = Column(Integer, ForeignKey('ARTISTE.artisteID'), primary_key=True)
+    groupeID = Column(Integer, ForeignKey('GROUPE.groupeID'), primary_key=True)
     concert = relationship(Concert)
-    artiste = relationship(Artiste)
+    groupe = relationship(Groupe)
 
-class Prepare(Base):
+class Prepare(db.Model):
     __tablename__ = 'PREPARE'
     personelTechniqueID = Column(Integer, ForeignKey('PERSONELTECHNIQUE.personelTechniqueID'), primary_key=True)
     concertID = Column(Integer, ForeignKey('CONCERT.concertID'), primary_key=True)
     concert = relationship(Concert)
     personelTechnique = relationship(PersonelTechnique)
 
-class Invitation(Base):
-    __tablename__ = 'INVITATION'
-    typePlaceID = Column(Integer, ForeignKey('TYPEPLACE.typePlaceID'), primary_key=True)
-    concertID = Column(Integer, ForeignKey('CONCERT.concertID'), primary_key=True)
-    quantiteInv = Column(Integer)
-    typePlace = relationship(TypePlace)
-    concert = relationship(Concert)
 
-class SalleTypePlace(Base):
+class SalleTypePlace(db.Model):
     __tablename__ = 'SALLETYPEPLACE'
     typePlaceID = Column(Integer, ForeignKey('TYPEPLACE.typePlaceID'), primary_key=True)
     salleID = Column(Integer, ForeignKey('SALLE.salleID'), primary_key=True)
@@ -207,7 +199,7 @@ class SalleTypePlace(Base):
     typePlace = relationship(TypePlace)
     salle = relationship(Salle)
 
-class Organiser(Base):
+class Organiser(db.Model):
     __tablename__ = 'ORGANISER'
     concertID = Column(Integer, ForeignKey('CONCERT.concertID'), primary_key=True)
     nomOrga = Column(String(50), ForeignKey('ORGANISATION.nomOrga'), primary_key=True)
